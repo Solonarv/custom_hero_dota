0bba7bf22606e183e6d59b8adc18bed3ef6d3338
Solonarv <nstamm@gmx.de>
2017-07-07 05:44:31 +0200

    Miscellaneous fixes:  - Ailil's Winter's Arrow slow/speed now displays as a single buff with a stack counter  - Fixed missing particle on Ailil's Rime Breath (by reusing Jakiro's Dual Breath (ice) particle)  - Fixed broken addoninfo.txt (hopefully)

---
+++ game/scripts/vscripts/heroes/hero_ailil/modifier_winters_arrow_slow.lua
@@ -0,0 +1,66 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: Solonarv
+-- Date: 07.07.2017
+-- Time: 01:37
+-- To change this template use File | Settings | File Templates.
+--
+
+function Setup(_)
+    LinkLuaModifier("modifier_winters_arrow_slow", "heroes/hero_ailil/modifier_winters_arrow_slow.lua", LUA_MODIFIER_MOTION_NONE)
+end
+
+modifier_winters_arrow_slow = class({})
+
+function modifier_winters_arrow_slow:DeclareFunctions()
+    local funcs = {
+        MODIFIER_PROPERTY_MOVESPEED_BONUS_CONSTANT
+    }
+    return funcs
+end
+
+function modifier_winters_arrow_slow:OnCreated(_)
+    self:Update()
+end
+
+function modifier_winters_arrow_slow:OnRefresh(_)
+    self:Update()
+end
+
+function modifier_winters_arrow_slow:Update()
+    print("Slow modifier update!")
+    if IsServer() then
+        local mods = self:GetParent():FindAllModifiersByName("modifier_winters_arrow_slow_hidden")
+        local modsFromCaster = {}
+        local duration = 0
+        local maxStacks = self:GetAbility():GetSpecialValueFor("max_stacks")
+        for _, mod in pairs(mods) do
+            if mod:GetCaster() == self:GetCaster() then
+                table.insert(modsFromCaster, mod)
+                local modDuration = mod:GetDuration()
+                if modDuration >= duration then
+                    duration = modDuration
+                end
+            end
+        end
+        table.sort(modsFromCaster, function(x, y) return x:GetDuration() < y:GetDuration() end)
+        while #modsFromCaster > maxStacks do
+            table.remove(modsFromCaster, 1)
+        end
+        if 0 == #modsFromCaster then
+            self:Destroy()
+        else
+            self:SetDuration(duration, true)
+            self:SetStackCount(#modsFromCaster)
+        end
+    end
+end
+
+
+function modifier_winters_arrow_slow:IsHidden()
+    return false
+end
+
+function modifier_winters_arrow_slow:GetModifierMoveSpeedBonus_Constant()
+    return -self:GetAbility():GetSpecialValueFor("movement_speed_stolen") * self:GetStackCount()
+end
